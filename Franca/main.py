e172541649385b168d8f153b2a74e81849e88994
Franca Tortarolo <155097175+FrancaTortaroloo@users.noreply.github.com>
2024-11-11 10:46:13 +0100

    actualizacion

--- Franca/main.py
+++ Franca/main.py
@@ -75,6 +75,10 @@ stop = llamada['total']
 for e in range(stop):
     print(e)
 #%%
+# reeemplazar el id en la llamada a la api y que extraiga los nombres de los tracks de cada album
+
+
+id_albums = [['494aUGKXMH5ruXtR3O1a3H'], ['1X472EvsSqH09RyrqbtyXU'], ['3Q9wXhEAX7NYCPP0hxIuDz'], ['28DUZ0itKISf2sr6hlseMy'], ['0zcNtUZ2oEpRmiDuWBFvcX'], ['5Eevxp2BCbWq25ZdiXRwYd'], ['6hU9JCoqq4GjYq86dQ1o9b'], ['3XB2yloP7l00tEUmaODtVi'], ['2FUsvD1bw53HGOjAg56vRD'], ['7qRKpkP0jSOlBMLYZhAMCh'], ['4XHIjbhjRmqWlosjj5rqSI'], ['0hJ3A7sih1AXDoMjXHLr7Q'], ['7IYqppCBhR5z9z8JqgXuxi'], ['4flcwtqnLoKZJ2wrCp1aJq'], ['1FiRqhpAowNK8gTl5sOhxZ'], ['4fy0SXW5G8evBCo2A3kn02'], ['5uvXx5ZQswNRFCdHR521YZ'], ['72seWTJF9U5SljizfyF2ZK'], ['4JtIVJRA342O0YoAchen5Q'], ['7pgs38iLfEqUtwgCRgvbND']]
 
 #diccionario para almacenar los id de los albumes con sus tracks
 
