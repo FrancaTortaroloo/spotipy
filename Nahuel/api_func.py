564308106946488eff03bba8f00cab7ccd1de4ea
Nahuel Nuñez <167609595+Nahuel-nunez@users.noreply.github.com>
2024-11-13 09:48:28 +0100

    actualizacion

--- Nahuel/api_func.py
+++ Nahuel/api_func.py
@@ -19,26 +19,23 @@ def preparamos_url(link):
 
 def obtener_tracks(sp, id_albums):
     
-    #creo un diccionario para almacenar los IDs de los albumes y los tracks
+    # Diccionario para almacenar los nombres de las canciones y sus IDs por álbum
     album_tracks = {}
-    
-    #extraer los nombres de los tracks de cada album
+
+    # Iterar por cada ID de álbum para obtener sus tracks
     for a_id in id_albums:
-        a_id = a_id[0] #extraer el id del album desde la lista 
-        tracks = sp.album_tracks(a_id)
-        
-        #creo una lista para lamacenar los nombres de las pistas en una lista
-        tracks_names = []
-        tracks_id = []
-        for t in tracks['items']:
-            tracks_names.append(t['name'])
-            tracks_id.append(t['id'])        
-        #asociar el ID del álbum con tracks_names
-        album_tracks[a_id] = tracks_names, tracks_id
-    
+        try:
+            # Obtener los tracks del álbum
+            tracks = sp.album_tracks(a_id)
+            album_tracks[a_id] = {t['name']: t['id'] for t in tracks['items']}
+        except Exception as e:
+            print(f"Error al obtener los tracks del álbum {a_id}: {e}")
+            album_tracks[a_id] = {}  # Almacena un álbum vacío si hay un error
+
     return album_tracks
 
-def song_album(album_id):
+
+def song_album(sp, album_id):
     
     # Obtiene las canciones del álbum
     results = sp.album_tracks(album_id)
@@ -49,19 +46,22 @@ def song_album(album_id):
 
     return track_dict
 
-def song_info(track_dict):
-    
+def song_info(sp, track_dict):
     # Lista para almacenar los IDs de las canciones
     track_ids = list(track_dict.values())
     
     # Llama al endpoint para obtener características de audio de varias canciones
     audio_features_list = sp.audio_features(tracks=track_ids)
 
-    # Construye un diccionario con el nombre de la canción y sus características de audio
+    # Claves que queremos eliminar
+    keys_to_discard = ['uri', 'track_href', 'analysis_url']
+
+    # Construye un diccionario con el nombre de la canción y sus características de audio, excluyendo las claves no deseadas
     audio_features_dict = {
-        track_name: features
+        track_name: {k: v for k, v in features.items() if k not in keys_to_discard}
         for track_name, features in zip(track_dict.keys(), audio_features_list)
     }
 
     return audio_features_dict
+
 
