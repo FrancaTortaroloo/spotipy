e30a607fb737db4dd0d9cfb48987f615cff0aa56
Franca Tortarolo <155097175+FrancaTortaroloo@users.noreply.github.com>
2024-11-21 10:09:09 +0100

    corrección de consultas

--- Franca/Consultas.sql
+++ Franca/Consultas.sql
@@ -1,8 +1,13 @@
 #Encontrar las canciones más bailables (danceability > 0.8)
+USE BD_linkin_park;
 
-SELECT track_features.track_name,
-track_features.danceability
+SELECT 
+track_features.track_name,
+track_features.danceability,
+artist_tracks.album
 FROM track_features
+JOIN artist_tracks
+ON artist_tracks.track_id = track_features.track_id
 WHERE track_features.danceability > 0.8
 ORDER BY track_features.danceability DESC;
 
@@ -23,15 +28,26 @@ ORDER BY promedio_duracion_canciones DESC;
 
 #5 canciones más largas
 
-SELECT track_name, duration_ms
+SELECT 
+track_features.track_name, 
+track_features.duration_ms,
+artist_tracks.album
 FROM track_features
+JOIN artist_tracks
+ON track_features.track_id = artist_tracks.track_id
 ORDER BY duration_ms DESC
 LIMIT 5; 
 
 
 #canción más melancólica
 
-SELECT track_name, artist, valence
+SELECT 
+track_features.track_name, 
+track_features.artist, 
+track_features.valence,
+artist_tracks.album
 FROM track_features 
+JOIN artist_tracks
+ON track_features.track_id = artist_tracks.track_id
 ORDER BY valence ASC
 LIMIT 1;
